b4e10d1bc543ae1ddd5d4706cceebd165a0c7a52
unknown <ahmadsandy313@gmail.com>
2023-08-10 11:03:17 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toogle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // Klik diluar sidebar untuk menghilangkan navbar //
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
