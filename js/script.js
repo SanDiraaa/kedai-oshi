05ea433957acad30ac80330d95db41f690b64cce
unknown <ahmadsandy313@gmail.com>
2023-08-28 18:48:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -6,11 +6,23 @@ document.querySelector("#hamburger-menu").onclick = (e) => {
   e.preventDefault();
 };
 
+// Ketika Member menu di klik
+const navbarList = document.querySelector(".navbar-list");
+
+document.querySelector("#member-button").onclick = (e) => {
+  navbarList.classList.toggle("active");
+  e.preventDefault();
+};
+
 // Klik diluar sidebar untuk menghilangkan navbar //
 const hamburger = document.querySelector("#hamburger-menu");
+const mb = document.querySelector("#member-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!mb.contains(e.target) && !navbarList.contains(e.target)) {
+    navbarList.classList.remove("active");
   }
 });
